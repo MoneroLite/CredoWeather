e73bda97225ba8efd71b6495c401638ffc59e482
MoneroLite <73478668+MoneroLite@users.noreply.github.com>
2022-12-01 23:18:47 +0500

    upd: weather footer

--- frontend/src/components/screens/WeatherFooter/WeatherFooter.tsx
+++ frontend/src/components/screens/WeatherFooter/WeatherFooter.tsx
@@ -1,9 +1,15 @@
 import React, { useContext } from "react";
 import { weatherContext } from "../../provider/weatherProvider/WeatherProvider";
 import styles from "./WeatherFooter.module.scss";
+import WeatherFooterSkeleton from "./weatherFooterSkeleton/WeatherFooterSkeleton";
 
 const WeatherFooter = () => {
   const [weather, isLoading] = useContext(weatherContext);
+
+  if (isLoading) {
+    return <WeatherFooterSkeleton />;
+  }
+
   return (
     <div className={styles.container}>
       <div>{weather?.wind.speed} м/с</div>
