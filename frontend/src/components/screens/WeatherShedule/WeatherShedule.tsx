4a07d02bc90785908be99d143cee06c2e5bcac68
MoneroLite <73478668+MoneroLite@users.noreply.github.com>
2022-12-19 00:13:32 +0500

    upd: weather shedule

--- frontend/src/components/screens/WeatherShedule/WeatherShedule.tsx
+++ frontend/src/components/screens/WeatherShedule/WeatherShedule.tsx
@@ -1,8 +1,80 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import styles from "./WeatherShedule.module.scss";
 
+interface ICity {
+  coord: { lat: number; lon: number };
+  country: string;
+  id: number;
+  name: string;
+  population: number;
+  sunrise: number;
+  sunset: number;
+  timezone: number;
+}
+interface IWeatherListWind {
+  speed: number;
+  deg: number;
+  gust: number;
+}
+interface IWeatherListWeather {
+  description: string;
+  icon: string;
+  id: number;
+  main: string;
+}
+interface IWeatherListMain {
+  feels_like: number;
+  grnd_level: number;
+  humidity: number;
+  pressure: number;
+  sea_level: number;
+  temp: number;
+  temp_kf: number;
+  temp_max: number;
+  temp_min: number;
+}
+interface IWeatherList {
+  clouds?: {};
+  dt: number;
+  dt_txt: string;
+  main: IWeatherListMain;
+  pop: number;
+  snow: [];
+  visibility: number;
+  weather: IWeatherListWeather[];
+  wind: IWeatherListWind;
+}
+interface IWeatherForecast {
+  city: ICity;
+  cnt: number;
+  cod: string;
+  list: IWeatherList[];
+  message: number;
+}
+
 const WeatherShedule = () => {
-  return <div></div>;
+  const [forecast, setForecast] = useState<IWeatherForecast>();
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.openweathermap.org/data/2.5/forecast?lang=ru&id=479561&units=metric&appid=b5f473f7704a8179247ce0e38148b13f&cnt=9"
+      )
+      .then(({ data }) => {
+        setForecast(data);
+        // setIsLoading(false);
+      });
+  }, []);
+
+  console.log(forecast);
+
+  return (
+    <div>
+      {forecast?.list.map((item) => (
+        <p>{item.dt_txt}</p>
+      ))}
+    </div>
+  );
 };
 
 export default WeatherShedule;
